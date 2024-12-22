fe322a6c7c5d66a669677db82613e4972c750200
benji2803 <benjamin.maldonado.556@my.csun.edu>
2024-12-22 03:05:07 -0800

    Added untracked files for portfolio components

--- src/Contact.js
+++ src/Contact.js
@@ -4,8 +4,9 @@ import './Contact.css'; // If the CSS file exists
 function Contact() {
   return (
     <div>
-      <h1>Contact Us</h1>
-      <p>This is the Contact component</p>
+      <h1>REACH ME HERE</h1>
+      <p>Email:benwebfl@gmail.com</p>
+      <p>Instagram:avi1003_</p>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import './Home.css'; // If the CSS file exists
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home component</h1>
+      <h1>POOOEPJOPNFWNFJEFPNFWJFN</h1>
     </div>
   );
 }
