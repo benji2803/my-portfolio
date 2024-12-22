7080344f8d325d68446c20e80fb94854d5ef42e8
benji2803 <benjamin.maldonado.556@my.csun.edu>
2024-12-22 02:47:39 -0800

    Added untracked files for portfolio components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-// App.js - Main Application File
-import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import About from './About';
 import Experience from './Experience';
 import Contact from './Contact';
-import './App.css'; // Optional, if you want to keep global styles.
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <Home />
-        <About />
-        <Experience />
-        <Contact />
-      </main>
+      <Home />
+      <About />
+      <Experience />
+      <Contact />
     </div>
   );
 }
