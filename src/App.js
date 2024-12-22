daf9c2788a8eeda65b449b6d3c5abcfa1ffb1cd8
benji2803 <benjamin.maldonado.556@my.csun.edu>
2024-12-22 02:56:28 -0800

    Added untracked files for portfolio components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './Header';
 import Home from './Home';
-import About from './About';
 import Experience from './Experience';
 import Contact from './Contact';
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      <About />
       <Experience />
       <Contact />
     </div>
