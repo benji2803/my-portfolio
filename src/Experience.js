136befbe7f2fc1bd012bfc27324b4942a2ba89c5
benji2803 <benjamin.maldonado.556@my.csun.edu>
2024-12-29 02:02:40 -0800

    spelling error

--- src/Experience.js
+++ src/Experience.js
@@ -6,8 +6,8 @@ function Experience() {
     <div>
       <h1>Experience</h1>
       <p>4th year COMP SCI/CIT student</p>
-      <p>3 years Mac OS maintnace</p>
-      <p>3 years server maitnance for small office group</p>
+      <p>3 years Mac OS maintenace</p>
+      <p>3 years server maintenance for small office group</p>
       <p>Afterschool club "Coding & more 101" for middle schoolers at Hale Charter Academy</p>
 
     
