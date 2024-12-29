1becec3287b3d3dfaea75cb174250b4da88d8c06
benji2803 <benjamin.maldonado.556@my.csun.edu>
2024-12-29 02:06:01 -0800

    se again

--- src/Experience.js
+++ src/Experience.js
@@ -6,7 +6,7 @@ function Experience() {
     <div>
       <h1>Experience</h1>
       <p>4th year COMP SCI/CIT student</p>
-      <p>3 years Mac OS maintenace</p>
+      <p>3 years Mac OS maintenance</p>
       <p>3 years server maintenance for small office group</p>
       <p>Afterschool club "Coding & more 101" for middle schoolers at Hale Charter Academy</p>
 
